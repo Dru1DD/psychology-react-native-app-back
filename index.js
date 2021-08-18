b0e144a4aaf417795921aaf6ddebe680fed5a409
Dru1DD <mrtproger@gmail.com>
2021-08-18 15:40:28 +0300

    Fix mongodb connect error

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/', indexRoute)
-
+// mongodb+srv://Dru1DD:v55PkyKqOXjir274@cluster0.bwimu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 const start = async() => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
